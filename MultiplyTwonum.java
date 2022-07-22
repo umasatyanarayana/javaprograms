c3ba47305802ca9a6f737dd61891d886596e5560
umasatyanarayana <umabalisetty400@gmail.com>
2022-07-22 14:56:55 +0530

    file added

---
+++ MultiplyTwonum.java
@@ -0,0 +1,7 @@
+public class MultiplyTwonum{
+public static void main(String args[]){
+int num1=34,num2=45,product;
+product=num1*num2;
+System.out.println("The product of given two numbers is: "+product);
+}
+}
